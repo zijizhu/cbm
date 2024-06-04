d69c98eb4847e423f4fe114cda1ea0f2fb347039
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-06-04 22:24:15 +1000

    Update experiment script settings

--- analysis.py
+++ analysis.py
@@ -290,7 +290,7 @@ def accuracy(output, target, topk=(1,)):
     batch_size = target.size(0)
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
-    temp = target.view(1, -1).expand_as(pred)
+    temp = target.reshape(1, -1).expand_as(pred)
     temp = temp.cuda()
     correct = pred.eq(temp)
 
